8886487c57b16987a34d9ffc14f83ed7700ec5bd
Hae-yong Lee <hyunggok@gumi.ac.kr>
2022-11-21 13:15:45 +0900

    교재 80p까지 버전

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo")
-    
+    """ pybo 목록 출력 """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
